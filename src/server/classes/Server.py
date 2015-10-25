1238d017536d654be9a222a027e9d441d97a3b3f
Luca Bertoni <luca.bertoni24@gmail.com>
2015-10-25 14:54:12 +0100

    Update server

--- src/server/classes/Server.py
+++ src/server/classes/Server.py
@@ -10,34 +10,6 @@ class Server():
 		self.oSocket = Socket(HOST,PORT)
 		self.oSocket.listen()
 	
-	def start(self):
-		nPid = os.getpid()
-
-		while 1:
-
-			# Se sono nel processo figlio elaboro la richiesta trasmessa dal padre
-			if nPid == 0:
-				bOk = self.elabora(oClient)
-
-				if not bOk:
-					LogBuffer.write("Errore durante l'elaborazione della richiesta",3)
-		
-				# Termino l'esecuzione del processo
-				os._exit(0)
-			else:
-				print("Ascolto...")
-
-				# Cosa fa				:				Attende di ricevere dei dati
-				# Ritorna				:				oClient -> tupla che identifica il client, così formata
-				# 										[0] => socket, oggetto socket riferito al client. Identifica la connessione con il client
-				# 										[1] => address
-				#											[0] => "192.168.0.183", stringa -> indirizzo del client
-				#											[1] => 50465, numerico -> porta del client
-				oClient = self.oSocket.accept()	# Resta in attesa di una connessione
-
-				# Una volta creata la connessione, creo un fork che elabora la richiesta
-				nPid = os.fork()
-
 	def parse(self,aData,oSock):
 		"""
 			Cosa fa			:			Esegue il parse del comando e reindirizza il funzionamento a funzioni specifiche
@@ -50,6 +22,11 @@ class Server():
 			Ritorna			:			bRet -> logico, true se tutto ok, false in caso di errore
 		"""
 		bRet = False
+
+		sComando = aData['sComando'].upper()
+
+		if sComando == 'LOGIN':
+			pass
 
 		return bRet
 
--- src/server/server.py
+++ src/server/server.py
@@ -1,7 +1,40 @@
 #!/usr/bin/python
 from classes.Server import Server
+from classes.LogBuffer import LogBuffer
 import os
 
 oServer = Server()
-oServer.start()
+
+nPid = os.getpid()
+
+while 1:
+
+	# Se sono nel processo figlio elaboro la richiesta trasmessa dal padre
+	if nPid == 0:
+		bOk = oServer.elabora(oClient)
+
+		if not bOk:
+			LogBuffer.write("Errore durante l'elaborazione della richiesta",3)
+
+		# Termino l'esecuzione del processo
+		os._exit(0)
+	else:
+		print("Ascolto...")
+		
+		try:
+			# Cosa fa				:				Attende di ricevere dei dati
+			# Ritorna				:				oClient -> tupla che identifica il client, così formata
+			# 										[0] => socket, oggetto socket riferito al client. Identifica la connessione con il client
+			# 										[1] => address
+			#											[0] => "192.168.0.183", stringa -> indirizzo del client
+			#											[1] => 50465, numerico -> porta del client
+			oClient = oServer.oSocket.accept()	# Resta in attesa di una connessione
+		except KeyboardInterrupt:
+			print("\nTermino l'esecuzione.")
+			oServer.close()
+			os._exit(0)
+
+		# Una volta creata la connessione, creo un fork che elabora la richiesta
+		nPid = os.fork()
+
 oServer.close()
